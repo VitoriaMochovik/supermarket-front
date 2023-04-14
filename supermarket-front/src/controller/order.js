511f37a06f52a3410c50c02fb48c8451cf16c2ec
Vitória Carolina Mochovik Mateus <mochovik.vc@gmail.com>
2023-04-14 16:20:50 -0300

    formatação

--- supermarket-front/src/controller/order.js
+++ supermarket-front/src/controller/order.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
 
 export async function createOrder(data) {
-
-
-    // await axios.post('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/order', data)
-
-    await axios.post('http://localhost:3001/order', data)
-    
+  await axios.post('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/order', data);
 }
--- supermarket-front/src/controller/product.js
+++ supermarket-front/src/controller/product.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
 export async function getAllProducts() {
-  // const result =  await axios.get('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/product')
-
-  const result = await axios.get('http://localhost:3001/product')
+  const result =  await axios.get('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/product')
 
   return result.data.sort((a, b) => {
     if (a.name <= b.name) return -1
