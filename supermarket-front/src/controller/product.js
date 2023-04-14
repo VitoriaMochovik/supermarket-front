418e205d438651f0a863e91e5b686ab9579f7f08
Vitória Carolina Mochovik Mateus <mochovik.vc@gmail.com>
2023-04-14 14:56:59 -0300

    ordenação dos produtos

--- supermarket-front/src/controller/product.js
+++ supermarket-front/src/controller/product.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
 export async function getAllProducts() {
+  // const result =  await axios.get('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/product')
 
+  const result = await axios.get('http://localhost:3001/product')
 
-    // const result =  await axios.get('http://ec2-44-201-181-250.compute-1.amazonaws.com:3001/product')
+  return result.data.sort((a, b) => {
+    if (a.name <= b.name) return -1
 
-    const result =  await axios.get('http://localhost:3001/product')
-    
-    return result.data
+    return 1
+  })
 
 }
